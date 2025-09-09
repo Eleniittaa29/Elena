dc778a08346724cf55b1baa146b6b96109292210
Eleniittaa29 <159139042+Eleniittaa29@users.noreply.github.com>
2025-09-10 00:46:03 +0200

    subiendo pagina

---
+++ psicologia.js
@@ -0,0 +1,29 @@
+  // Función para redirigir al contacto
+  function irAContacto() {
+    window.location.href = "contacto.html";
+}
+
+// Función para redirigir al inicio
+function irAInicio() {
+  
+        window.location.href = "index.html";
+    }
+
+
+// Función para redirigir a la página "Sobre mí"
+function irASobreMi() {
+    window.location.href = "sobremi.html";
+}
+
+function irAInfo1() {
+    window.location.href = "info1.html";
+}
+function irAInfo2() {
+    window.location.href = "info2.html";
+}
+function irAInfo3() {
+    window.location.href = "info3.html";
+}
+function navigateTo(page) {
+    window.location.href = page; // Redirige a la página indicada
+}
---
+++ sobremi.js
@@ -0,0 +1,11 @@
+// Función para redirigir al contacto
+function irAContacto() {
+    window.location.href = "contacto.html";
+}
+
+function irAInicio() {
+    window.location.href = "Psicologia.html";
+}
+function irASobreMi(){
+    window.location.href = "sobremi.html";
+}
