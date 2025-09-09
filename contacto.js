d2ece5e1d211a0507ad325da220cba0b62b2ceb4
Eleniittaa29 <159139042+Eleniittaa29@users.noreply.github.com>
2025-09-10 00:59:04 +0200

    Add files via upload
    subir pagina

---
+++ contacto.js
@@ -0,0 +1,10 @@
+
+    // Función para redirigir al contacto
+    function irAContacto() {
+        window.location.href = "contacto.html";
+    }
+
+    function irAInicio() {
+        window.location.href = "Psicologia.html";
+    }
+
